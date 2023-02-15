5ff869cf2cdb764b14d2b78948dbfbfc903d70ca
Akam Programist <akadev404@gmail.com>
2023-02-15 20:17:09 +0300

    commits

--- resources/views/loans/index.blade.php
+++ resources/views/loans/index.blade.php
@@ -1,6 +1,6 @@
 @include('partials._hero')
 <div class="p-5 mb-5 py-10">
-    <p class="text-3xl text-center mb-5 font-bold">Loan For Businesses</p>
+    <p id="loan" class="text-3xl text-center mb-5 font-bold">Loan For Businesses</p>
     <div class="flex overflow-auto snap-x">
         @unless(count($loans) == 0)
             @foreach ($loans as $loan)
--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -1,4 +1,4 @@
-  <div class="relative">
+  {{-- <div class="relative">
       <img class="brightness-50 md:object-cover md:w-full md:max-h-[500px]"
           src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTYxfHxoZWxwJTIwYnVzaW5lc3N8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
           alt="" />
@@ -14,4 +14,38 @@
               View Online Events
           </button>
       </div>
-  </div>
+  </div> --}}
+  <section class="bg-white">
+      <div class="grid py-8 px-5 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div class="place-self-center mr-auto lg:col-span-7">
+              <h1 class="mb-5 text-gray-700 max-w-2xl text-4xl font-extrabold tracking-wide md:text-5xl xl:text-6xl">
+                  Make a loan,<br /> <span
+                      class="text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 to-green-600">
+                      change a
+                      life
+                  </span></h1>
+              <p class="mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">Kiva is a loan, not a
+                  donation, allowing you to cycle your money and create a personal impact across the world. Plus, you
+                  can
+                  withdraw your funds at any time.</p>
+              <a href="#"
+                  class="inline-flex justify-center items-center py-3 px-5 mr-3 text-base font-medium text-center text-white rounded-lg bg-green-600 hover:bg-green-700 focus:ring-4 focus:ring-green-300 ">
+                  Find Opportunities
+                  <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd"
+                          d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                          clip-rule="evenodd"></path>
+                  </svg>
+              </a>
+              <a href="#"
+                  class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
+                  Help A Business
+              </a>
+          </div>
+          <div class="mt-8 lg:mt-0 lg:col-span-5 lg:flex">
+              <img
+                  src="https://img.freepik.com/free-vector/refund-concept-illustration_114360-6803.jpg?w=740&t=st=1676464934~exp=1676465534~hmac=4bd339b8c6d08713cd577c3654380e0cb2d2a7cfe13c96255cf03414f43d3f6d">
+          </div>
+      </div>
+  </section>
